831852d0f7b5ec23539a5292a2d8dfa2920d998b
im-samwong <samuel.wong60@gmail.com>
2024-09-17 17:26:25 -0400

    Completed Orders List features for assignment 1

--- src/Drivers/OrdersDriver.cpp
+++ src/Drivers/OrdersDriver.cpp
@@ -1,3 +1,39 @@
 //
 // Created by samue on 2024-09-12.
 //
+
+#include "../Orders/Orders.h"
+
+namespace Orders {
+    void testOrdersLists() {
+        // Create an OrderList object
+        OrderList orderList;
+
+        // Add all types of orders to the list
+        orderList.addOrder(std::make_unique<DeployOrder>());
+        orderList.addOrder(std::make_unique<AdvanceOrder>());
+        orderList.addOrder(std::make_unique<BombOrder>());
+        orderList.addOrder(std::make_unique<BlockadeOrder>());
+        orderList.addOrder(std::make_unique<AirliftOrder>());
+        orderList.addOrder(std::make_unique<NegotiateOrder>());
+
+        // Print the orders in the list
+        std::cout << "Orders in list:" << std::endl;
+        orderList.printOrders();
+
+        // Move the second order to the first position
+        orderList.moveOrder(1, 0);
+        std::cout << "Moving orders from index 1 to index 0" << std::endl;
+        std::cout << "Orders after moving:" << std::endl;
+        orderList.printOrders();
+
+        // Remove the first order
+        orderList.removeOrder(0);
+        std::cout << "Removing order at index 0" << std::endl;
+        std::cout << "Orders after removal:" << std::endl;
+        orderList.printOrders();
+
+        // Execute remaining orders
+        orderList.executeOrders();
+    }
+}
--- src/Orders/Orders.h
+++ src/Orders/Orders.h
@@ -5,12 +5,106 @@
 #ifndef ORDERS_H
 #define ORDERS_H
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
 
+namespace Orders {
 
-class Orders {
+    // Abstract Order class
+    class Order {
+    public:
+        // Default destructor for Order class
+        virtual ~Order() = default;
 
-};
+        // Abstract methods to be implemented by subclasses
+        virtual bool validate() = 0;
+        virtual void execute() = 0;
 
+        // Stream insertion operator to describe the order so when you do << object of type Order, it will print immediately
+        friend std::ostream& operator<<(std::ostream& os, const Order& order) {
+            return os << "Order: " << order.description();
+        }
 
+    protected:
+        virtual std::string description() const = 0;  // For printing the order
+    };
 
-#endif //ORDERS_H
+    // Subclass: DeployOrder
+    class DeployOrder : public Order {
+    public:
+        bool validate() override;
+        void execute() override;
+
+    protected:
+        std::string description() const override;
+    };
+
+    // Subclass: AdvanceOrder
+    class AdvanceOrder : public Order {
+    public:
+        bool validate() override;
+        void execute() override;
+
+    protected:
+        std::string description() const override;
+    };
+
+    // Subclass: BombOrder
+    class BombOrder : public Order {
+    public:
+        bool validate() override;
+        void execute() override;
+
+    protected:
+        std::string description() const override;
+    };
+
+    // Subclass: BlockadeOrder
+    class BlockadeOrder : public Order {
+    public:
+        bool validate() override;
+        void execute() override;
+
+    protected:
+        std::string description() const override;
+    };
+
+    // Subclass: AirliftOrder
+    class AirliftOrder : public Order {
+    public:
+        bool validate() override;
+        void execute() override;
+
+    protected:
+        std::string description() const override;
+    };
+
+    // Subclass: NegotiateOrder
+    class NegotiateOrder : public Order {
+    public:
+        bool validate() override;
+        void execute() override;
+
+    protected:
+        std::string description() const override;
+    };
+
+    // OrderList class to manage a list of orders
+    class OrderList {
+    public:
+        void addOrder(std::unique_ptr<Order> order);  // Add an order
+        void removeOrder(int index);                  // Remove an order by index
+        void moveOrder(int fromIndex, int toIndex);   // Move an order in the list
+        void executeOrders();                         // Execute all valid orders
+        void printOrders() const;                     // Print the list of orders
+
+    private:
+        std::vector<std::unique_ptr<Order>> orders;  // Vector of smart pointers to orders
+    };
+
+}
+
+#endif // ORDERS_H
+
