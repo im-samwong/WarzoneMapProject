8cee1ab467733230fd35aae407dcd85b70ebea6f
RenalWasouf <123764157+RenalWasouf@users.noreply.github.com>
2024-09-17 21:58:03 -0400

    Update Cards.h
    Formatting

--- src/Cards/Cards.h
+++ src/Cards/Cards.h
@@ -5,50 +5,50 @@
 #include <string>
 #include <iostream>
 
-namespace Cards{
-class Card {
-public:
-    enum CardType { BOMB, REINFORCEMENT, BLOCKADE, AIRLIFT, DIPLOMACY };
-    Card(CardType type); // Constructor
-    Card(const Card& other);  // Copy constructor
-    ~Card(); // Destructor
+namespace Cards {
+    class Card {
+    public:
+        enum CardType { BOMB, REINFORCEMENT, BLOCKADE, AIRLIFT, DIPLOMACY };
+        Card(CardType type); // Constructor
+        Card(const Card& other);  // Copy constructor
+        ~Card(); // Destructor
+        
+        Card& operator=(const Card& other);  // Assignment operator
+        
+        std::string getTypeAsString() const; // Get the card type as a string
+        void play(Orders::OrderList* orderList);  // Play the card and return it to the deck
     
-    Card& operator=(const Card& other);  // Assignment operator
+        // Overloaded stream operator to print card details
+        friend std::ostream& operator<<(std::ostream& out, const Card& card);
     
-    std::string getTypeAsString() const; // Get the card type as a string
-    void play(Orders::OrderList* orderList);  // Play the card and return it to the deck
-
-    // Overloaded stream operator to print card details
-    friend std::ostream& operator<<(std::ostream& out, const Card& card);
-
-private:
-    CardType* type;  // Pointer type for class member
-};
-
-class Deck {
-public:
-    Deck(); // Constructor
-    ~Deck(); // Destructor
-
-    std::vector<Card*> getCards(); // Get the deck of cards
-    Card* draw();  // Draw a random card from the deck
+    private:
+        CardType* type;  // Pointer type for class member
+    };
     
-private:
-    std::vector<Card*> cards;  // Pointer type for each card in the deck
-};
-
-class Hand {
-public:
-    Hand(); // Constructor
-    ~Hand(); // Destructor
+    class Deck {
+    public:
+        Deck(); // Constructor
+        ~Deck(); // Destructor
     
-    std::vector<Card*> getHandCards(); // Get the cards in the hand
-    void addCard(Card* card); // Add a card to the hand
-    void playCard(Card* card, Deck* deck, Orders::OrderList* orderList); // Play a card from the hand
+        std::vector<Card*> getCards(); // Get the deck of cards
+        Card* draw();  // Draw a random card from the deck
+        
+    private:
+        std::vector<Card*> cards;  // Pointer type for each card in the deck
+    };
     
-private:
-    std::vector<Card*> handCards;  // Pointer type for each card in the hand
-};
+    class Hand {
+    public:
+        Hand(); // Constructor
+        ~Hand(); // Destructor
+        
+        std::vector<Card*> getHandCards(); // Get the cards in the hand
+        void addCard(Card* card); // Add a card to the hand
+        void playCard(Card* card, Deck* deck, Orders::OrderList* orderList); // Play a card from the hand
+        
+    private:
+        std::vector<Card*> handCards;  // Pointer type for each card in the hand
+    };
 }
 
 #endif
