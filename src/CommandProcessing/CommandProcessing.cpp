071eb94e78a796f3a190ac0945e4f55bd9e86a6d
binal011 <105316220+binal011@users.noreply.github.com>
2024-10-31 18:16:49 -0400

    Command processor and command adapter

---
+++ src/CommandProcessing/CommandProcessing.cpp
@@ -0,0 +1,158 @@
+//
+// Created by Binal Patel on 2024-10-15.
+//
+#include "CommandProcessing.h"
+#include <vector>
+#include <string>
+#include <iostream>
+
+Command::Command(const std::string& cmd, const std::string& arg)
+    : command(cmd), argument(arg) {}
+
+void Command::saveEffect(const std::string& effect) {
+    this->effect = effect;
+}
+
+
+void CommandProcessor::getCommand(const std::string& cmd) {
+    std::string commandName = cmd.substr(0, cmd.find(" "));
+    std::string argument = (cmd.find(" ") != std::string::npos) ? cmd.substr(cmd.find(" ") + 1) : "";
+
+    Command command(commandName, argument);
+
+    if (validate(command)) {
+        command.saveEffect("The command '" + commandName + "' is valid!");
+        std::cout << "Current game state: " + currentGameState << std::endl;
+        saveCommand(command);
+
+        updateGameState(command.getCommand());
+    } else {
+        command.saveEffect("ERROR! The command '" + commandName + "' is invalid!\n Current game state: " + currentGameState);
+        saveCommand(command);
+    }
+}
+
+Command CommandProcessor::getLastCommand() const {
+    return *lc.back(); //returns last command
+}
+
+bool CommandProcessor::validate(Command& command) {
+    static const std::unordered_map<std::string, std::vector<std::string>> validCommands = {
+        {"start", {"loadmap"}},
+        {"maploaded", {"loadmap", "validatemap"}},
+        {"mapvalidated", {"addplayer"}},
+        {"playersadded", {"addplayer", "gamestart"}},
+        {"assignreinforcement", {"issueorder"}},
+        {"issueorders", {"issueorder", "issueordersend"}},
+        {"executeorders", {"executeorder", "endexecorders", "win"}},
+        {"win", {"replay", "quit"}}
+    };
+
+    const auto& validCommandsForCurrentState = validCommands.find(currentGameState);
+    bool isValid = false;
+
+    if (validCommandsForCurrentState != validCommands.end()) {
+        for (const std::string& validCommand : validCommandsForCurrentState->second) {
+            if (command.getCommand() == validCommand) {
+                if ((validCommand == "loadmap" || validCommand == "addplayer") && command.getArgument().empty()) {
+                    if (validCommand == "loadmap") {
+                        std::cout << "The command '"+ validCommand + "' requires a file argument (i.e. loadmap fileName.map)" << std::endl;
+                    } else {
+                        std::cout << "The command '"+ validCommand + "' requires a name argument (i.e. addplayer playerName)" << std::endl;
+                    }
+
+                    isValid = false;
+                    break;
+                } else {
+                    isValid = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (!isValid) {
+        command.saveEffect("For the current game state '" + currentGameState + "' the command '" + command.getCommand() + "' is invalid!");
+    }
+
+    return isValid;
+}
+
+void CommandProcessor::readCommand(const std::string& cmd) {
+    std::string command;
+    std::string argument;
+
+    size_t space = cmd.find(' ');
+    if (space != std::string::npos) {
+        command = cmd.substr(0, cmd.find(" "));
+        argument = cmd.substr(cmd.find(" ") + 1);
+    } else {
+        command = cmd; // for commands that are not loadmap or addplayer
+    }
+
+    lc.push_back(std::make_unique<Command>(command, argument));
+    updateGameState(command);
+}
+
+void CommandProcessor::saveCommand(const Command& command) {
+    auto commandCopy = std::make_unique<Command>(command);
+    lc.push_back(std::move(commandCopy));
+}
+
+void CommandProcessor::updateGameState(const std::string& command) {
+    if (command.find("loadmap") == 0) {
+        currentGameState = "maploaded";
+    }
+    else if (command == "validatemap") {
+        currentGameState = "mapvalidated";
+    }
+    else if (command.find("addplayer") == 0) {
+        currentGameState = "playersadded";
+    }
+    else if (command == "gamestart") {
+        currentGameState = "assignreinforcement";
+    }
+    else if (command == "issueorder") {
+        currentGameState = "issueorders";
+    }
+    else if (command == "issueordersend") {
+        currentGameState = "executeorders";
+    }
+    else if (command == "execorder") {
+        currentGameState = "executeorders";
+    }
+    else if (command == "endexecorders") {
+        currentGameState = "assignreinforcement";
+    }
+    else if (command == "win") {
+        currentGameState = "win";
+    }
+    else if (command == "replay") {
+        currentGameState = "start";
+    }
+    else if (command == "quit") {
+        currentGameState = "exit";
+    }
+}
+
+
+FileLineReader::FileLineReader(const std::string& file) : inputFile(file) {
+    if (!inputFile.is_open()) {
+        throw std::runtime_error("Error! The file '" + file + "' could not be opened");
+    }
+}
+
+bool FileLineReader::readLineFromFile(std::string& line) {
+    return std::getline(inputFile, line) ? true : false;
+}
+
+
+FileCommandProcessorAdapter::FileCommandProcessorAdapter(const std::string& fileName)
+    : flr(fileName) {}
+
+void FileCommandProcessorAdapter::readCommand() {
+    std::string line;
+    while (flr.readLineFromFile(line)) {
+        getCommand(line);
+    }
+}
---
+++ src/CommandProcessing/CommandProcessing.h
@@ -0,0 +1,74 @@
+//
+// Created by Binal Patel on 2024-10-15.
+//
+#ifndef COMMANDPROCESSING_H
+#define COMMANDPROCESSING_H
+
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+
+class Command {
+public:
+    //Constructors
+    Command(const std::string& cmd, const std::string& arg);
+    //Methods
+    void saveEffect(const std::string& effect);
+    std::string getCommand() const {return command;}
+    std::string getEffect() const {return effect;}
+    std::string getArgument() const {return argument;}
+
+private:
+    //Attributes
+    std::string command;
+    std::string effect;
+    std::string argument;
+};
+
+
+class CommandProcessor {
+public:
+    //Constructor
+    CommandProcessor() : currentGameState("start") {}
+    //Methods
+    void getCommand(const std::string& cmd);
+    bool validate(Command& command);
+    Command getLastCommand() const;
+
+private:
+    //Methods
+    void readCommand(const std::string& cmd);
+    void saveCommand(const Command& command);
+    void updateGameState(const std::string& command);
+    //Attributes
+    std::string currentGameState;
+    std::vector<std::unique_ptr<Command>> lc; //stores collection of command objects
+};
+
+
+class FileLineReader {
+public:
+    //Constructor
+    FileLineReader(const std::string& file);
+    //Method
+    bool readLineFromFile(std::string& line);
+
+private:
+    std::ifstream inputFile;
+};
+
+
+class FileCommandProcessorAdapter : public CommandProcessor {
+public:
+    //Constructor
+    FileCommandProcessorAdapter(const std::string& fileName);
+    //Method
+    void readCommand();
+
+private:
+    FileLineReader flr;
+};
+
+#endif // COMMANDPROCESSING_H
