c15b5e0db82f2dd7be420e0c8185b6424c2d0f07
DanielC2001 <daniel-carcamo1@hotmail.com>
2024-11-14 16:04:55 -0500

    Created PlayerStrategy and TODOs for AggressivePlayer

--- src/PlayerStrategies/PlayerStrategies.cpp
+++ src/PlayerStrategies/PlayerStrategies.cpp
@@ -1,3 +1,53 @@
-//
-// Created by danielm on 11/13/24.
-//
+#include "PlayerStrategies.h"
+#include "../Player/Player.h"
+
+Player* PlayerStrategy::getPlayer() const {
+    return player;
+}
+
+PlayerStrategy::PlayerStrategy(Player* player, const std::string& description) {
+    //No need to own this player just use the ptr
+    this->player = player;
+    this->description = new std::string(description);
+}
+
+PlayerStrategy& PlayerStrategy::operator=(const PlayerStrategy &other) {
+    if (this != &other) {
+        player = other.player;
+    }
+    return *this;
+}
+
+PlayerStrategy::PlayerStrategy(const PlayerStrategy &other) {
+    player = other.player;
+}
+
+std::string PlayerStrategy::getDescription() const {
+    return *description;
+}
+
+std::ostream& operator<<(std::ostream& os, const PlayerStrategy& strategy) {
+    os << "PlayerStrategy: " << strategy.description << std::endl;
+    os << "Player using this strategy: " << strategy.player->getName() << std::endl;
+
+    return os;
+}
+
+AggressivePlayer::AggressivePlayer(Player* player): PlayerStrategy(player, "AggressivePlayer") {}
+
+AggressivePlayer::~AggressivePlayer() {
+    //DO NOT DELETE PLAYER AS WE DON'T OWN IT
+    delete description;
+}
+
+void AggressivePlayer::issueOrder(std::vector<Player *> *players) {
+    //TODO:
+}
+
+void AggressivePlayer::toAttack() {
+    //TODO:
+}
+
+void AggressivePlayer::toDefend() {
+    //TODO:
+}
--- src/PlayerStrategies/PlayerStrategies.h
+++ src/PlayerStrategies/PlayerStrategies.h
@@ -1,5 +1,6 @@
 #ifndef PLAYERSTRATEGIES_H
 #define PLAYERSTRATEGIES_H
+#include <ostream>
 #include <vector>
 
 class Player;
@@ -7,12 +8,43 @@ class Player;
 class PlayerStrategy {
 protected:
     Player* player;
+    std::string* description{};
 
 public:
-    virtual issueOrder(std::vector<Player*>* players) = 0;
-    virtual toAttack() = 0;
-    virtual toDefend() = 0;
+    //issueOrder with other players parameter because certain commands need Player infos
+    virtual void issueOrder(std::vector<Player*>* players) = 0;
+    virtual void toAttack() = 0;
+    virtual void toDefend() = 0;
     virtual ~PlayerStrategy() = 0;
 
+    //Getter for obtaining the player using this strategy
+    Player* getPlayer() const;
+
+    std::string getDescription() const;
+
+    //Main constructor
+    PlayerStrategy(Player* player, const std::string& description);
+
+    //Copy constructor
+    PlayerStrategy(const PlayerStrategy& other);
+
+    //Assignment operator
+    PlayerStrategy& operator=(const PlayerStrategy& other);
+
+    //Stream insertion operator
+    friend std::ostream& operator<<(std::ostream& os, const PlayerStrategy& strategy);
+};
+
+class AggressivePlayer: public PlayerStrategy {
+public:
+    AggressivePlayer(Player* player);
+
+    void issueOrder(std::vector<Player *> *players) override;
+
+    void toAttack() override;
+
+    void toDefend() override;
+
+    ~AggressivePlayer() override;
 };
 #endif //PLAYERSTRATEGIES_H
